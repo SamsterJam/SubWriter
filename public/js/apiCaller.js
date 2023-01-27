9f5694d8f7862f99e71b2a022d1a6e6dc0075058
Mossy7777 <nomedidio@gmail.com>
2023-01-26 21:28:45 -0500

    1.0.0 Release

--- public/js/apiCaller.js
+++ public/js/apiCaller.js
@@ -1,151 +1,4 @@
-
-
-if(window.innerHeight/window.innerWidth > 1){
-    var menu = document.getElementById("menu");
-    menu.style="visibility:hidden; min-width:0px; width:0px;";
-}
-
-
-
-addEventListener("resize", (event) => {
-    if(window.innerHeight/window.innerWidth > 1){
-        var menu = document.getElementById("menu");
-        menu.style="visibility:hidden; min-width:0px; width:0px;";
-    }else{
-        var menu = document.getElementById("menu");
-        menu.style="visibility:visable; min-width:200px; width:200px;";
-    }
-});
-
-
-
-
-
-
-
-
-
-function clearGhostText(){
-    document.getElementById("ghost-text").value='';
-}
-
-
-document.getElementById('prompt').addEventListener('keydown', function(e) {
-	if(e.code == 'Enter' && e.ctrlKey) {
-		apiCallTestSubmit()
-	}
-});
-
-
-//Temerature Slider
-var slider1 = document.getElementById("temperature-selection");
-var output1  = document.getElementById("selected-temp-value");
-output1.innerHTML = slider1.value;
-slider1.oninput = function() {
-    output1.innerHTML = slider1.value;
-}
-
-//Length Slider
-var slider2 = document.getElementById("length-selection");
-var output2  = document.getElementById("selected-length-value");
-output2.innerHTML = slider2.value;
-slider2.oninput = function() {
-    output2.innerHTML = slider2.value;
-}
-
-//Freq Slider
-var slider3 = document.getElementById("frequency-selection");
-var output3  = document.getElementById("selected-frequency-value");
-output3.innerHTML = slider3.value;
-slider3.oninput = function() {
-    output3.innerHTML = slider3.value;
-}
-
-//Pres Slider
-var slider4 = document.getElementById("presence-selection");
-var output4  = document.getElementById("selected-presence-value");
-output4.innerHTML = slider4.value;
-slider4.oninput = function() {
-    output4.innerHTML = slider4.value;
-}
-
-
-
-function hideKey(){
-    document.getElementById("API-Key").type="password";
-}
-
-function showKey(){
-    document.getElementById("API-Key").type="text";
-}
-
-function apiCallTestSubmit(){
-    document.getElementById("prompt").disabled = true;
-    document.getElementById("prompt").style="background-color: #1d1e23;";
-    document.getElementsByClassName("loader")[0].style="visibility: visible";
-    var button = document.getElementById("submit-button");
-    button.disabled = true;
-    button.style="background: linear-gradient(174deg, #6c56b3 0%, #483683 100%);";
-
-    
-    // Get the values of the input fields
-    var model = document.getElementById("model-selection");
-    var modelChoice = model.options[model.selectedIndex].text
-
-    var temp = document.getElementById("temperature-selection").value;
-    var length = document.getElementById("length-selection").value;
-    var frequency = document.getElementById("frequency-selection").value;
-    var presence = document.getElementById("presence-selection").value;
-    var prompt = document.getElementById("prompt").value.toString();
-    var apiKey = document.getElementById("API-Key").value.toString();
-    // Get the values of the other input fields
-
-    fetch('/apiCall', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "model": modelChoice, "prompt": prompt, "temperature": parseFloat(temp), "length": parseInt(length), "frequency": parseFloat(frequency), "presence": parseFloat(presence), "apiKey": apiKey})
-    })
-    .then(response => response.json())
-    .then(response => updateWithData(response))
-    
-
-};
-
-
-function updateWithData(response){
-    if(response.choices[0].text == ''){
-        document.getElementById("ghost-text").value = document.getElementById("prompt").value + ' [NO COMPLEATION]';
-    }else if(response.choices[0].text == 'API-ERROR'){
-        document.getElementById("ghost-text").value = document.getElementById("prompt").value + ' [API ERROR]';
-    }else{
-        document.getElementById("prompt").value += response.choices[0].text;
-    }
-
-    var prompt = document.getElementById("prompt");
-    prompt.disabled = false;
-    prompt.style="background-color: #2b2f38;";
-    prompt.scrollTop = prompt.scrollHeight;
-
-    document.getElementsByClassName("loader")[0].style="visibility: hidden";
-
-    var button = document.getElementById("submit-button");
-    button.disabled=false;
-    button.style="background: linear-gradient(174deg, #9a7bfd 0%, #7558d2 100%);";
-    
-}
-
-
-
-
-
-
-
-
-
-//keep API key
+//Arguments
 function parseQuery(str) {
     //Remove '?' from beginning.
     str = str.substring(1) 
@@ -163,6 +16,112 @@ function parseQuery(str) {
 
 
 var query = window.location.search
-var keyInfo = parseQuery(query);
+var queryData = parseQuery(query);
 
-document.getElementById('API-Key').value = keyInfo.APIkeyid;
+var apiKey = queryData.key;
+var temp = (queryData.temp != undefined)? queryData.temp : 0.3;
+var length = (queryData.len != undefined)? queryData.len : 100;
+var frequency = (queryData.freq != undefined)? queryData.freq : 1.5;
+
+
+
+
+
+function clearGhostText(){
+    document.getElementById("ghost-text").value='';
+    document.getElementById("ghost-text").style.opacity = 0.5;
+}
+
+
+document.getElementById('prompt').addEventListener('keydown', function(e) {
+	if((e.code == 'Enter' && e.ctrlKey) || (e.code == 'Enter' && e.metaKey)) {
+		apiCallTestSubmit()
+	}
+});
+
+
+//make tabs work
+document.getElementById('prompt').addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
+
+
+function hideKey(){
+    document.getElementById("API-Key").type="password";
+}
+
+function showKey(){
+    document.getElementById("API-Key").type="text";
+}
+
+function apiCallTestSubmit(){
+
+    //Disable prompt
+    var promptText = document.getElementById("prompt");
+    promptText.disabled = true;
+    promptText.style="background-color: #17171a;"
+    prompt = promptText.value;
+
+    //set color of loader to random
+    var loadColors = ['#98c379', '#61afef', '#c678dd', '#de6a73', '#d19a66'];
+    var random_color = loadColors[Math.floor(Math.random() * loadColors.length)];
+    var loader = document.getElementsByClassName("loader")[0];
+    loader.firstElementChild.style.background = random_color;
+
+
+    document.getElementsByClassName("loader")[0].style="visibility: visible";
+
+    fetch('/apiCall', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "model": 'text-davinci-003', "prompt": prompt, "temperature": parseFloat(temp), "length": parseInt(length), "frequency": parseFloat(frequency), "presence": parseFloat(frequency), "apiKey": apiKey})
+    })
+    .then(response => response.json())
+    .then(response => updateWithData(response))
+    
+
+};
+
+
+function updateWithData(response){
+    if(response.choices[0].text == ''){
+        document.getElementById("ghost-text").value = document.getElementById("prompt").value+ ' [NO COMPLEATION]';
+    }else if(response.choices[0].text == 'API-ERROR'){
+        document.getElementById("ghost-text").value = document.getElementById("prompt").value + ' [API ERROR]';
+    }else{
+        document.getElementById("prompt").value += String(response.choices[0].text);
+
+    }
+
+    var promptObj = document.getElementById("prompt");
+    promptObj.disabled = false;
+    promptObj.style="background-color: #202124;";
+    prompt.scrollTop = prompt.scrollHeight;
+
+    document.getElementsByClassName("loader")[0].style="visibility: hidden";
+    
+}
+
+
+
+
+
+
+
+
+
